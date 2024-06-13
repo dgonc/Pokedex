fdda6f565a0e0a4968167e3ff1f5ca99f075a8df
dgonc <d.goncalves89000@gmail.com>
2024-06-13 15:04:05 +0200

    add buttons to switch pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,45 @@
 import './App.css'
 import PokemonCard from './components/PokemonCard';
+import { useState } from 'react';
 
 const pokemonList = [
   {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 
 function App() {
 
+  const [pokemonIndex, setPokemonIndex]= useState(0);
+
   return (
   <div>
-    {pokemonList.map((pokemon) => {
-      return <PokemonCard pokemon={pokemon}/>}
-    )}
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+    {pokemonIndex > 0 ? <button onClick={() => setPokemonIndex(pokemonIndex-1)}>Précédent</button> : <></>}
+    {pokemonIndex < pokemonList.length - 1 ? <button onClick={() => setPokemonIndex(pokemonIndex+1)}>Suivant</button>: <></> }
   </div>
   );
 }
 
 export default App
-
