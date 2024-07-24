d41c805914d2e28bfe52a4ab7653dc46cad0623b
dgonc <d.goncalves89000@gmail.com>
2024-07-24 16:18:13 +0200

    add alert on start and on click on Pikachu

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,51 @@
-import './App.css'
-import Navbar from './components/Navbar';
-import PokemonCard from './components/PokemonCard';
-import { useState } from 'react';
+import "./styles/app.css";
+import Navbar from "./components/Navbar";
+import PokemonCard from "./components/PokemonCard";
+import { useEffect, useState } from "react";
 
 const pokemonList = [
   {
-      id:1,
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      id:2,
-      name: "charmander",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-    },
-    {
-      id:3,
-      name: "squirtle",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-    },
-    {
-      id:4,
-      name: "pikachu",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-    },
-    {
-      id:5,
-      name: "mew",
-    },
-  ];
-
+    id: 1,
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    id: 2,
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    id: 3,
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    id: 4,
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    id: 5,
+    name: "mew",
+  },
+];
 
 function App() {
+  useEffect(() => alert("Hello pokemon trainer :)"), []);
 
-  const [pokemonIndex, setPokemonIndex]= useState(0);
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+  console.log("index dans Apps", pokemonIndex);
 
   return (
-  <div>
+    <div className="card">
       <PokemonCard pokemon={pokemonList[pokemonIndex]} />
-      <Navbar navigation={[pokemonIndex, setPokemonIndex]} poke={pokemonList} />
-  </div>
+      <Navbar setPokemonIndex={setPokemonIndex} poke={pokemonList} />
+    </div>
   );
 }
 
-export default App
+export default App;
